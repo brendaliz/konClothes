f80206913ea64a5f3d211cb41e553597f239a7a4
CarlosAOspinoI <30841434+CarlosAOspinoI@users.noreply.github.com>
2018-09-10 23:14:52 -0500

    Intents y Transitions between login and register interfaces

--- app/src/main/java/co/edu/konradlorenz/konclothes/Login.java
+++ app/src/main/java/co/edu/konradlorenz/konclothes/Login.java
@@ -1,13 +1,40 @@
 package co.edu.konradlorenz.konclothes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    private Button btnRegistrarse;
+    private Button btnAcceder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnRegistrarse = (Button) findViewById(R.id.btnRegistrarse);
+        btnAcceder = (Button) findViewById(R.id.btnAcceder);
+
+        btnRegistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, register.class);
+                startActivity(intent);
+            }
+        }
+        );
+
+        //This intent needs to be tested
+        /*btnAcceder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });*/
     }
 }
--- app/src/main/java/co/edu/konradlorenz/konclothes/register.java
+++ app/src/main/java/co/edu/konradlorenz/konclothes/register.java
@@ -1,13 +1,38 @@
 package co.edu.konradlorenz.konclothes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class register extends AppCompatActivity {
+
+    private Button btnAcceder;
+    private Button btnRegresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        btnAcceder = (Button) findViewById(R.id.btnAcceder);
+        btnRegresar = (Button) findViewById(R.id.btnRegresar);
+
+        btnAcceder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(register.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRegresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(register.this,Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
