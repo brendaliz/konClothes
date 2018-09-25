f00c18e8c833b90c9b58332e8ab9e07bb48f3bd1
brendaliz <brendacanov@gmail.com>
2018-09-25 13:35:38 -0500

    mofications in content main
    Index Page

--- app/src/main/java/co/edu/konradlorenz/konclothes/MainActivity.java
+++ app/src/main/java/co/edu/konradlorenz/konclothes/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -17,6 +18,9 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private ViewPager viewPager;
+    private SlideAdapter myadapter;
+
 
 
     @Override
@@ -25,6 +29,9 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        myadapter = new SlideAdapter(this);
+        viewPager.setAdapter(myadapter);
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
