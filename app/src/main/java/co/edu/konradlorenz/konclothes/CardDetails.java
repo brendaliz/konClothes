f487d2b33e2fe52cbff630016d1ba7051a75ed33
CarlosAOspinoI <30841434+CarlosAOspinoI@users.noreply.github.com>
2018-10-23 02:20:05 -0500

    Fixes in texts and intents
    A big review to the project's intents and texts has been made. Every change or additions to the strings, or styles it's been submitted in this commit.

--- app/src/main/java/co/edu/konradlorenz/konclothes/CardDetails.java
+++ app/src/main/java/co/edu/konradlorenz/konclothes/CardDetails.java
@@ -1,13 +1,38 @@
 package co.edu.konradlorenz.konclothes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CardDetails extends AppCompatActivity {
+
+    private Button btnRegresarDetCompra;
+    private Button btnComprar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_details);
+
+        btnRegresarDetCompra = (Button) findViewById(R.id.btnRegresarDetCompra);
+        btnComprar = (Button) findViewById(R.id.btnComprar);
+
+        btnRegresarDetCompra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CardDetails.this,PurchaseDetails.class);
+                startActivity(intent);
+            }
+        });
+
+        btnComprar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CardDetails.this,Purchases.class);
+                startActivity(intent);
+            }
+        });
     }
 }
